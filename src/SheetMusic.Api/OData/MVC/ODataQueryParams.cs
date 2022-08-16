4e354f6d8abab8d0f9a94720b0852caa7b5ace67
Leif Bjarte Johansson <leif.bjarte.johansson@innovasjonnorge.no>
2022-08-16 22:53:23 +0200

    Fixing the HasBeenScanned flag (#5)
    * Added dependabot scanning for nuget
    
    * Fixed issue where HasBeenScanned = false when $expand=parts is not included. Also added request duration in Swagger + fixed issue with OData model binding.

--- src/SheetMusic.Api/OData/MVC/ODataQueryParams.cs
+++ src/SheetMusic.Api/OData/MVC/ODataQueryParams.cs
@@ -1,17 +1,29 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.Json.Serialization;
 
 namespace SheetMusic.Api.OData.MVC;
 
 [ModelBinder(typeof(ODataParamResolver))]
 public class ODataQueryParams
 {
+    [JsonPropertyName("$top")]
     public int? Top { get; set; }
+
+    [JsonPropertyName("$skip")]
     public int? Skip { get; set; }
+
+    [JsonPropertyName("$orderBy")]
     public List<ODataOrderByOption> OrderBy { get; set; } = new List<ODataOrderByOption>();
+
+    [JsonPropertyName("$filter")]
     public ODataExpression? Filter { get; set; }
+    
+    [JsonPropertyName("$search")]
     public string? Search { get; set; }
+
+    [JsonPropertyName("$expand")]
     public List<string> Expand { get; set; } = new List<string>();
 
     public bool HasFilter => Filter != null;
