004bdbfaf1bd4631d5bf58e2eb7b22b496e8a89b
Leif Bjarte Johansson <leif@odinsoftware.no>
2026-07-01 13:55:03 +0200

    feat: rate limit forgot-password and login endpoints (#170)

--- src/SheetMusic.Api/Configuration/ConfigKeys.cs
+++ src/SheetMusic.Api/Configuration/ConfigKeys.cs
@@ -8,4 +8,8 @@ public static class ConfigKeys
     public const string ResendApiKey = "Resend:ApiKey";
     public const string EmailFromAddress = "Email:FromAddress";
     public const string EmailFrontendBaseUrl = "Email:FrontendBaseUrl";
+    public const string RateLimitingForgotPasswordPermitLimit = "RateLimiting:ForgotPassword:PermitLimit";
+    public const string RateLimitingForgotPasswordWindowSeconds = "RateLimiting:ForgotPassword:WindowSeconds";
+    public const string RateLimitingTokenPermitLimit = "RateLimiting:Token:PermitLimit";
+    public const string RateLimitingTokenWindowSeconds = "RateLimiting:Token:WindowSeconds";
 }
