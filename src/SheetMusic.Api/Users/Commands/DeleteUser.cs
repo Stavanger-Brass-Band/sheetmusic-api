5236c26b2203439c3f6ff522a46032c9f62eddf4
Leif Bjarte Johansson <lejo@equinor.com>
2026-07-30 10:33:13 +0200

    fix(users): detach connected musician before hard-deleting a user

--- src/SheetMusic.Api/Users/Commands/DeleteUser.cs
+++ src/SheetMusic.Api/Users/Commands/DeleteUser.cs
@@ -1,5 +1,7 @@
 using MediatR;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using SheetMusic.Api.Database;
 using SheetMusic.Api.Database.Entities;
 using SheetMusic.Api.Errors;
 using SheetMusic.Api.Users.Errors;
@@ -15,12 +17,15 @@ public class DeleteUser(Guid userId, bool hardDelete = false) : IRequest
     public Guid UserId { get; } = userId;
     public bool HardDelete { get; } = hardDelete;
 
-    public class Handler(UserManager<ApplicationUser> userManager) : IRequestHandler<DeleteUser>
+    public class Handler(UserManager<ApplicationUser> userManager, SheetMusicContext db) : IRequestHandler<DeleteUser>
     {
         public async Task Handle(DeleteUser request, CancellationToken cancellationToken)
         {
             var user = await userManager.FindByIdAsync(request.UserId.ToString())
                 ?? throw new NotFoundError($"users/{request.UserId}", "User not found");
+
+            if (request.HardDelete)
+                await DetachMusicianAsync(request.UserId, cancellationToken);
 
             var result = request.HardDelete
                 ? await userManager.DeleteAsync(user)
@@ -33,6 +38,16 @@ public class DeleteUser(Guid userId, bool hardDelete = false) : IRequest
             {
                 userToDeactivate.Inactive = true;
                 return await userManager.UpdateAsync(userToDeactivate);
+            }
+
+            async Task DetachMusicianAsync(Guid applicationUserId, CancellationToken ct)
+            {
+                var musician = await db.Musicians.SingleOrDefaultAsync(m => m.ApplicationUserId == applicationUserId, ct);
+                if (musician is null)
+                    return;
+
+                musician.ApplicationUserId = null;
+                await db.SaveChangesAsync(ct);
             }
         }
     }
